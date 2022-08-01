67ab06fd2799a21b8f261dd64d44f75c93789770
p-iknow <apricotsoul@gmail.com>
2022-08-01 20:20:29 +0900

    feat: api path edit

--- hooks/useName/index.ts
+++ hooks/useName/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
 
-export const API_PATH = 'http://localhost:3000/api/name';
+export const API_PATH = '/api/name';
 
 const fetchName = async () => {
   const { data } = await axios.get<{ name: string }>(API_PATH);
